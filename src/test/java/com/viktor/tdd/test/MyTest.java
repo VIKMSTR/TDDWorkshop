f9c9c4dec0c916981a65b36148b015151e0e59ba
z003e2nc <z003e2nc@MD1ERM5C.ww300.siemens.net>
2016-12-06 17:39:21 +0100

    Added hamcrest example and wiki link

--- src/test/java/com/viktor/tdd/test/MyTest.java
+++ src/test/java/com/viktor/tdd/test/MyTest.java
@@ -1,5 +1,7 @@
 package com.viktor.tdd.test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
@@ -7,6 +9,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.viktor.tdd.TestedClass;
+/***
+ * 
+ * @author z003e2nc
+ *
+ * Hamcrest wiki: https://code.google.com/archive/p/hamcrest/wikis/Tutorial.wiki
+ */
 public class MyTest
 {
 
@@ -32,6 +40,7 @@ public class MyTest
     tested.setField(5);
     assertEquals(5, tested.getField());
     assertEquals(6, tested.sum(3, 3));
+    assertThat("sum",tested.sum(3, 3), equalTo(6));
   }
 
 }
