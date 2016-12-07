7cd5b34d3232c54774795eb0d45cc3a2bdafc624
z003e2nc <z003e2nc@MD1ERM5C.ww300.siemens.net>
2016-12-07 13:13:11 +0100

    created test for fracion

---
+++ src/main/java/com/viktor/tdd/Fraction.java
@@ -0,0 +1,55 @@
+package com.viktor.tdd;
+
+import lombok.Getter;
+
+
+public class Fraction
+{
+
+  @Getter
+  private final int nominator;
+  @Getter
+  private final int denominator;
+ 
+  
+  public Fraction(int number){
+    nominator = number;
+    denominator = 1;
+  }
+  public Fraction(int nominator, int denominator){
+    this.nominator = nominator;
+    this.denominator = denominator;
+  }
+  
+  public Fraction plus(Fraction f) throws NumberFormatException{
+    int resultNominator;
+    int resultDenominator = denominator * f.denominator;
+    
+    if(isInvalidNumber(this) || isInvalidNumber(f)){
+      throw new NumberFormatException("Not a valid number");
+    }
+    if(isZeroNumber(this)){
+      return new Fraction(f.nominator,f.denominator);
+    }
+    if(isZeroNumber(f)){
+      return new Fraction(nominator,denominator);
+    }
+    
+    resultNominator = resultDenominator / denominator * nominator + resultDenominator / f.denominator * f.nominator;
+    return new Fraction(resultNominator, resultDenominator);
+  }
+
+  
+  private static boolean isInvalidNumber(Fraction f ){
+    return f.getDenominator() == 0 && f.getNominator() != 0;
+  }
+  
+  private static boolean isZeroNumber(Fraction f ){
+    return f.denominator == 0 && f.nominator == 0;
+  }
+  
+  public double toDecadic(){
+    return nominator / (double) denominator;
+    
+  }
+}
--- src/test/java/com/viktor/tdd/MyTest.java
+++ src/test/java/com/viktor/tdd/MyTest.java
@@ -2,13 +2,9 @@ package com.viktor.tdd;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
-
-import com.viktor.tdd.TestedClass;
 /***
  * 
  * @author z003e2nc
@@ -19,6 +15,7 @@ public class MyTest
 {
 
   private TestedClass tested;
+  private Fraction testedFraction;
   public MyTest()
   {
     // TODO Auto-generated constructor stub
@@ -27,20 +24,65 @@ public class MyTest
   @Before
   public void prepare(){
     tested = new TestedClass();
+    
   }
-  
+      
+  @Test
+  public void zeroFractionwSum(){
+    testedFraction = new Fraction(0, 0);
+    assertThat("Is Initialized", testedFraction.getNominator(), equalTo(0));
+    assertThat("Is Initialized", testedFraction.getDenominator(), equalTo(0));
+    
+    Fraction result = testedFraction.plus(testedFraction);
+    assertThat("Zero sum", result.getNominator(), equalTo(0));
+    assertThat("zero sum", result.getDenominator(), equalTo(0));
+  }
   
   @Test
-  public void failingTest(){
-    fail("Failed");
+  public void addFractions(){
+    testedFraction = new Fraction(2, 3);
+    Fraction addedFraction = new Fraction(4,2);
+    Fraction result = testedFraction.plus(addedFraction);
+    assertThat("sum", result.getNominator(), equalTo(16));
+    assertThat("sum", result.getDenominator(), equalTo(6));
   }
   
   @Test
-  public void sumTest(){
-    tested.setField(5);
-    assertEquals(5, tested.getField());
-    assertEquals(6, tested.sum(3, 3));
-    assertThat("sum",tested.sum(3, 3), equalTo(6));
+  public void addFractionWithZero(){
+    testedFraction = new Fraction(2, 3);
+    Fraction addedFraction = new Fraction(0,0);
+    Fraction result = testedFraction.plus(addedFraction);
+    assertThat("Zero sum", result.getNominator(), equalTo(2));
+    assertThat("zero sum", result.getDenominator(), equalTo(3));
   }
+  
+  @Test(expected=NumberFormatException.class)
+  public void tryToAddInvalidNumber(){
+    testedFraction = new Fraction(2, 3);
+    Fraction addedFraction = new Fraction(6,0);
+    Fraction result = testedFraction.plus(addedFraction);
+    assertThat("Zero sum", result.getNominator(), equalTo(2));
+    assertThat("zero sum", result.getDenominator(), equalTo(3));
+  }
+  
+  @Test
+  public void testDecadic(){
+    testedFraction = new Fraction(1, 2);
+    Fraction addedFraction = new Fraction(1,2);
+    Fraction result = testedFraction.plus(addedFraction);
+    assertThat("sum", result.toDecadic(), equalTo(1.0));
+    
+  }
+  
+  @Test
+  public void testOverflow(){
+    testedFraction = new Fraction(1, 1);
+    Fraction addedFraction = new Fraction(Integer.MAX_VALUE,Integer.MAX_VALUE);
+    Fraction result = testedFraction.plus(addedFraction);
+    assertThat("sum", result.toDecadic(), equalTo(2.0));
+    
+  }
+  
+
 
 }
