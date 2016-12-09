89a537751ee5829d0b079b6347c84b13843bbefb
z003e2nc <z003e2nc@MD1ERM5C.ww300.siemens.net>
2016-12-09 10:15:28 +0100

    Added JB's main method. So it is runnable.

---
+++ src/main/java/com/viktor/tdd/fluffycloud/jb/PointOfSaleTerminal.java
@@ -0,0 +1,43 @@
+package com.viktor.tdd.fluffycloud.jb;
+
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.viktor.tdd.fluffycloud.ConsoleDisplay;
+import com.viktor.tdd.fluffycloud.Price;
+import com.viktor.tdd.fluffycloud.test.DummyCatalog;
+
+public class PointOfSaleTerminal
+{
+
+  private static Map<String, Price> prices;
+
+  public PointOfSaleTerminal()
+  {
+ 
+  
+  }
+
+  public static void main(String[] args)
+  { 
+    fillPrices();
+    new ConsumeTextCommands(
+                           new SellOneItemController(
+                                                     new DummyCatalog("EUR", prices ),
+                                                     new ConsoleDisplay()
+                                                    )::onBarcode //you see dat amazing method reference, don't you ?
+                            ).consume(
+                                      new InputStreamReader(System.in)
+                            );
+
+  }
+
+  private static void fillPrices()
+  {
+    prices = new HashMap<>();
+    prices.put("123456", new Price("EUR", 100));
+    prices.put("7070529026686", new Price("EUR",1.25));
+  }
+
+}
